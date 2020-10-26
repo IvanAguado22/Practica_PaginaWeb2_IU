f021e42353aa9fbfc3975887c3b48e0d9dda2c7a
JorgeSerrano <100405987@alumnos.uc3m.es>
2020-10-26 12:53:27 +0100

    Añadida funcionalidad descargar tabla

--- ej/script/script.js
+++ ej/script/script.js
@@ -29,9 +29,28 @@ function mostrar4() {
 function cerrarSesion() {
     var txt;
     if (confirm("Seguro que quieres cerrar sesión?")) {
-      txt = "You pressed OK!";
+        
     } else {
       txt = "You pressed Cancel!";
     }
     document.getElementById("demo").innerHTML = txt;
-  }
+}
+
+function enviarEmail(){
+    window.open('mailto:test@example.com');
+}
+
+function descargarExcel(){
+    // Creamos un Elemento Temporal en forma de enlace
+    var tmpElemento = document.createElement('a');
+    // Obtenemos la información desde el div que lo contiene en el html
+    // Obtenemos la información de la tabla
+    var data_type = 'data:application/vnd.ms-excel';
+    var tabla_div = document.getElementById('tabla');
+    var tabla_html = tabla_div.outerHTML.replace(/ /g, '%20');
+    tmpElemento.href = data_type + ', ' + tabla_html;
+    // Asignamos el nombre a nuestro EXCEL
+    tmpElemento.download = 'calificaciones.xls';
+    // Simulamos el click al elemento creado para descargarlo
+    tmpElemento.click();
+}
