8b1f3450015bb1e21c909fffb5bd8ccb686a77dd
JorgeSerrano <100405987@alumnos.uc3m.es>
2020-11-02 11:43:19 +0100

    Funcionalidad cookies

--- ej/script/pagInicio.js
+++ ej/script/pagInicio.js
@@ -1,4 +1,58 @@
+function checkCookie(){
+    var inputEmail = document.getElementById("emailLogin").value;
+    var cookieEmail = getCookie("email");
+    var inputPass = document.getElementById("passLogin").value;
+    var cookiePass = getCookie("pass");
+    if(inputEmail == cookieEmail){
+        if(inputPass == cookiePass){
+            verificarPassword();
+        } else{
+            alert("La contraseña es incorrecta");
+        }
+    } else{
+        alert("Este correo electrónico no está dado de alta");
+    }
+}
 
+function deleteCookie(){
+    document.cookie = "name= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function saveCookies(){
+    setCookie("name", document.getElementById("name").value, 30);
+    setCookie("surname", document.getElementById("surname").value, 30);
+    setCookie("usrname", document.getElementById("usrname").value, 30);
+    setCookie("nia", document.getElementById("nia").value, 30);
+    setCookie("bornDate", document.getElementById("bornDate").value, 30);
+    setCookie("dni", document.getElementById("dni").value, 30);
+    setCookie("rol", document.getElementById("rol").value, 30);
+    setCookie("grade", document.getElementById("grade").value, 30);
+    setCookie("lenguage", document.getElementById("lenguage").value, 30);
+    setCookie("email", document.getElementById("email").value, 30);
+    setCookie("pass", document.getElementById("pass").value, 30);
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 function alertCookie() {
     alert(document.cookie);
@@ -6,7 +60,7 @@ function alertCookie() {
 
 //***************************************************************************************************************** */
 function verificarPassword(){
-    var inputPassword = document.getElementById('pass').value;
+    var inputPassword = document.getElementById('passLogin').value;
     if (inputPassword.length < 8) {
         alert("too_short");
         return("too_short");
@@ -19,7 +73,6 @@ function verificarPassword(){
     }
     document.getElementById("pagInicio").style.display = "none";
     document.getElementById("pagWeb").style.display = "block";
-    return("ok");
 }
 
 function cambiarLogIn(){
